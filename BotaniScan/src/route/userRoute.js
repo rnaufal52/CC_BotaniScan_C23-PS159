491db897622a4f6af39c072aa5fe3ed67fd8b827
Rifqi Naufal <116421845+rnaufal52@users.noreply.github.com>
2023-06-07 14:51:27 +0700

    Update userRoute.js

--- BotaniScan/src/route/userRoute.js
+++ BotaniScan/src/route/userRoute.js
@@ -1,11 +1,12 @@
 import express from 'express'
 import { userValidate } from '../validation/userSchema.js'
+import { passwordValidate } from '../validation/passwordSchema.js'
 import { validate } from '../middleware/validate.js'
 import auth from '../middleware/authentication.js'
 
 // memanggil controller about
 import {
-    getByTokenUser, updateUser
+    getByTokenUser, updateUser, changePassword
 } from '../controller/userController.js'
 
 const router = express.Router()
@@ -17,5 +18,8 @@ router.get('/users', auth, getByTokenUser)
 
 // UPDATE DATA
 router.put('/users', auth, validate(userValidate), updateUser)
+
+// change password
+router.put('/users/changePassword', auth, validate(passwordValidate), changePassword)
 
 export default router 
